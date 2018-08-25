62d9a624adfe954549bc98636cd737001d44951f
Fazle <ahmed.belal98@gmail.com>
2018-08-25 23:25:36 +0530

    Practice

--- At Coder/Regular 090/E.cpp
+++ At Coder/Regular 090/E.cpp
@@ -21,9 +21,81 @@ using vvi = vector<vector<int>> ;
 using vlli = vector<long long int> ;
 using vpii = vector<pair<int, int>> ;
 
+lli n, m, u, d, v, w, s, t, x, D, ans ;
+vector<tuple<lli, lli, lli>> edges ;
+vlli dpS, dpT, distS, distT ;
+vector<pair<lli, lli>> temp ;
+vector<vector<pair<lli, lli>>> adj ;
+priority_queue<pair<lli, lli>, vector<pair<lli, lli>>, greater<pair<lli, lli>>> pq ;
+
+void init()
+  {
+    adj.resize(n+1) ;
+    dpS.assign(n+1, 0) ; dpT.assign(n+1, 0) ;
+    distS.assign(n+1, INF) ; distT.assign(n+1, INF) ;
+  }
+
+void dijsktra(int start, vlli &dist, vlli &dp)
+  {
+    dist[start]=0 ;
+    dp[start]=1 ;
+    pq.push(mp(0LL, start)) ;
+    while(!pq.empty())
+      {
+        tie(d, u)=pq.top() ; pq.pop() ;
+        if(d>dist[u]) continue ;
+        for(auto v:adj[u])
+          if(dist[u]+v.se<dist[v.fi])
+            {
+              dist[v.fi]=dist[u]+v.se ;
+              pq.push(mp(dist[v.fi], v.fi)) ;
+              dp[v.fi]=dp[u] ;
+            }
+          else if(dist[u]+v.se==dist[v.fi]) dp[v.fi]=(dp[v.fi]+dp[u])%mod ;
+      }
+  }
+
+void calc()
+  {
+    D=distS[t] ;
+    assert(distS[t]==distT[s]) ;
+    ans=(dpS[t]*dpS[t])%mod ;
+    for(int i=1 ; i<=n ; i++)
+      if(distS[i]==D/2 && distT[i]==D/2)
+        {
+          x=(dpS[i]*dpT[i])%mod ;
+          ans=(ans-(x*x)%mod+mod)%mod ;
+        }
+    for(auto i:edges)
+      {
+        tie(u, v, w)=i ;
+        if(distS[u]<D/2 && distT[v]<D/2 && distS[u]+distT[v]+w==D)
+          {
+            x=(dpS[u]*dpT[v])%mod ;
+            ans=(ans-(x*x)%mod+mod)%mod ;
+          }
+        if(distS[v]<D/2 && distT[u]<D/2 && distS[v]+distT[u]+w==D)
+          {
+            x=(dpS[v]*dpT[u])%mod ;
+            ans=(ans-(x*x)%mod+mod)%mod ;
+          }
+      }
+  }
+
 int main()
   {
     ios_base::sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0) ;
-    for(int i=0 ; (1<<i)<=1024 ; i++)
-      cout<< i << endl ;
-    }
+    cin>> n >> m >> s >> t ;
+    init() ;
+    for(int i=1 ; i<=m ; i++)
+      {
+        cin>> u >> v >> w ;
+        adj[u].pb(mp(v, w)) ;
+        adj[v].pb(mp(u, w)) ;
+        edges.pb(mt(u, v, w)) ;
+      }
+    dijsktra(s, distS, dpS) ;
+    dijsktra(t, distT, dpT) ;
+    calc() ;
+    cout<< ans ;
+  }
